0f7767a73572590d17cdfafa9e797436164f48f3
Lilith <lilit.asatryan.ec@gmail.com>
2023-05-23 08:08:54 +0400

    19-05

---
+++ fswd/19-05-2023/script.js
@@ -0,0 +1,25 @@
+const url = 'http://localhost:8081';
+fetch('users.json')
+  .then(response => {
+    if (response.status !== 200) {
+      throw new Error('Failed to fetch data. Response status: ' + response.status);
+    }
+    return response.json();
+  })
+  .then(data => {
+    const users = data.users;
+
+    const userId = parseInt(window.prompt('Enter user ID:')); 
+    const user = users.find(user => user.id === userId);
+
+    if (user) {
+      console.log('User:', user);
+    } else {
+      console.log('User not found.');
+    }
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+
