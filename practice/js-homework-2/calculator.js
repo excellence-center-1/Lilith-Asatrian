12a60e4623b4bb04c2ebe68926478de99ff013ee
Lilith Asatrian <lilit.asatryan.ec@gmail.com>
2025-02-10 21:35:06 +0400

    changes

--- practice/js-homework-2/calculator.js
+++ practice/js-homework-2/calculator.js
@@ -1,38 +1,35 @@
 const prompt = require('prompt-sync')();
+
 const calculator = (number1, number2, operation) => {
     switch (operation) {
         case "+":
             return number1+number2;
-            break;
         case "-":
             return number1-number2;
-            break;
         case "*":
             return number1*number2;
-            break;
         case "/":
             return number1/number2;
-            break;
     }
 }
 
 try {
-    const number1 = parseInt(prompt("Input first number: "));
-    if(isNaN(number1)){
-        throw new Error("The input should be number!")
+    const number1 = prompt("Input first number: ");
+    if(number1 === "" || !isFinite(number1)){
+        throw new Error("The input should be valid number!")
     }
-    const number2 = parseInt(prompt("Input second number: "));
-    if(isNaN(number2)){
-        throw new Error("The input should be number!")
+    const number2 = prompt("Input second number: ");
+    if(number2 === "" || !isFinite(number2)){
+        throw new Error("The input should be valid number!")
     }
     const operation = prompt("Input operation: ");
     if(!['+', '-', '/', '*'].includes(operation)){
         throw new Error("No such operation!")
     }
-    if(operation=='/' && number2==0){
+    if(operation=='/' && Number(number2) == 0){
         throw new Error("No division to zero!")
     }
-    console.log(`Result: ${calculator(number1, number2, operation)}`);
+    console.log(`Result: ${calculator(Number(number1), Number(number2), operation)}`);
 } catch(error){
     console.error("Error: ", error.message);
 }
