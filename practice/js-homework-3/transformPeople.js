542bc46198540b7f9267a6b06170d2ce196946d9
Lilith Asatrian <lilit.asatryan.ec@gmail.com>
2025-02-13 12:38:50 +0400

    js-homework-3

--- practice/js-homework-3/transformPeople.js
+++ practice/js-homework-3/transformPeople.js
@@ -1,9 +1,3 @@
-const reverse = (str) => {
-  for (let index = 0; index < str.length / 2; index++) {
-    str[index] = str[str.lenght - index - 1];
-  }
-  return str;
-};
 const transformPeople = (people) => {
   for (let index = 0; index < people.length; index++) {
     people[index]["name"] = people[index]["name"].toUpperCase();
